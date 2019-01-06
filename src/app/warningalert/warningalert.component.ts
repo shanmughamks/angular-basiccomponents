31e6014cd88354cba9536dffc0193b6cfe1e7b1c
ShanmughamKS <ks_shan@hotmail.com>
2019-01-05 23:19:12 -0500

    Initial Commit

---
+++ src/app/warningalert/warningalert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "../../../node_modules/@angular/core";
+
+@Component({
+    selector: 'app-warningalert',
+    templateUrl: './warningalert.component.html',
+    styleUrls: ['./warningalert.component.css']
+})
+export class WarningAlertComponent {
+    
+}
